4686e0f5c4ad1800a0721a3bac87ae5c361b34be
leonardo munoz 🌑🌍 <168220131+leonardomunooz@users.noreply.github.com>
2024-11-09 20:16:36 +0000

    se ha creado la estructura del login

--- src/front/js/component/Login.jsx
+++ src/front/js/component/Login.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 const Login = () => {
     return (
-        <h1> This is the login</h1>
+
+
+        <div className="container mx-auto " style={{ "width": "800px" }}>
+            <div className="row">
+                <div className="col-12">
+                    <div className="mb-3">
+                        <label for="formGroupEmail" className="form-label">Email</label>
+                        <input type="text" className="form-control" id="formGroupEmail" placeholder="Example input email" />
+                    </div>
+                </div>
+                <div className="col-12">
+                    <div className="mb-3">
+                        <label for="formGroupPassword" className="form-label">Password</label>
+                        <input type="text" className="form-control" id="formGroupPassword" placeholder="Another input password" />
+                    </div>
+                </div>
+                <div className="col-12">
+                    <div className="col-12">
+                        <button type="submit" className="btn btn-primary">Sign in</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
